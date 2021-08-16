6d59cf0053bd785609e963cf6afd1ca24a0ef3eb
tomoki1230 <tomoki_1230@yahoo.co.jp>
2021-08-17 00:29:02 +0900

    bootstrap機能の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,22 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap/dist/js/bootstrap';
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+document.addEventListener('turbolinks:load', () => {
+  const search = document.getElementById('language-search');
+  const tableRows = document.querySelectorAll('#language-tbody tr');
+
+  const filterTableRows = (e) => {
+    const value = e.target.value.toLowerCase();
+    for (const tr of tableRows) {
+      const isVisible = tr.textContent.toLowerCase().includes(value);
+      tr.style.display = isVisible ? '' : 'none';
+    }
+  };
+
+  search.addEventListener('input', filterTableRows);
+});
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+note_params = [
+  {
+    language: "HTML/CSS",
+    description: "Webサイトの画面を作るための言語"
+  },
+  {
+    language: "JavaScript",
+    description: "Webサイトの画面に動きをつけるための言語"
+  },
+  {
+    language: "Ruby",
+    description: "Webサイトの裏側の処理をする言語"
+  },
+  {
+    language: "PHP",
+    description: "Webサイトの裏側の処理をする言語"
+  },
+  {
+    language: "Python",
+    description: "人工知能を開発するための言語"
+  }
+]
+
+Note.delete_all
+Note.create!(note_params)
+puts "初期データの投入に成功しました！"
